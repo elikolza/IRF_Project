c9df7443d2d191b38c53b8d22a4920083d4ee424
Elza <elza.koleszar@stud.uni-corvinus.hu>
2020-12-10 13:09:02 +0100

    Inicializálás

--- tokcz/tokcz/Form1.Designer.cs
+++ tokcz/tokcz/Form1.Designer.cs
@@ -28,34 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1main = new System.Windows.Forms.Panel();
-            this.button2birds = new System.Windows.Forms.Button();
+            this.buttoncsv = new System.Windows.Forms.Button();
+            this.buttonexcel = new System.Windows.Forms.Button();
+            this.dgw = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1main
+            // buttoncsv
             // 
-            this.panel1main.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1main.AutoSize = true;
-            this.panel1main.Location = new System.Drawing.Point(22, 318);
-            this.panel1main.Name = "panel1main";
-            this.panel1main.Size = new System.Drawing.Size(878, 446);
-            this.panel1main.TabIndex = 5;
+            this.buttoncsv.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.buttoncsv.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttoncsv.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttoncsv.Font = new System.Drawing.Font("Calibri", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttoncsv.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.buttoncsv.Location = new System.Drawing.Point(22, 251);
+            this.buttoncsv.Name = "buttoncsv";
+            this.buttoncsv.Size = new System.Drawing.Size(105, 50);
+            this.buttoncsv.TabIndex = 3;
+            this.buttoncsv.Text = "CSV file beolvasása";
+            this.buttoncsv.UseVisualStyleBackColor = false;
             // 
-            // button2birds
+            // buttonexcel
             // 
-            this.button2birds.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.button2birds.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2birds.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2birds.Font = new System.Drawing.Font("Calibri", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button2birds.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button2birds.Location = new System.Drawing.Point(22, 251);
-            this.button2birds.Name = "button2birds";
-            this.button2birds.Size = new System.Drawing.Size(202, 50);
-            this.button2birds.TabIndex = 3;
-            this.button2birds.Text = "Madárfaj hozzáadása";
-            this.button2birds.UseVisualStyleBackColor = false;
+            this.buttonexcel.BackColor = System.Drawing.Color.Goldenrod;
+            this.buttonexcel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonexcel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonexcel.Font = new System.Drawing.Font("Calibri", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonexcel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.buttonexcel.Location = new System.Drawing.Point(147, 251);
+            this.buttonexcel.Name = "buttonexcel";
+            this.buttonexcel.Size = new System.Drawing.Size(105, 50);
+            this.buttonexcel.TabIndex = 6;
+            this.buttonexcel.Text = "Formázott excel";
+            this.buttonexcel.UseVisualStyleBackColor = false;
+            // 
+            // dgw
+            // 
+            this.dgw.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw.Location = new System.Drawing.Point(22, 319);
+            this.dgw.Name = "dgw";
+            this.dgw.RowHeadersWidth = 62;
+            this.dgw.RowTemplate.Height = 28;
+            this.dgw.Size = new System.Drawing.Size(892, 457);
+            this.dgw.TabIndex = 8;
             // 
             // Form1
             // 
@@ -63,20 +78,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(926, 788);
-            this.Controls.Add(this.button2birds);
-            this.Controls.Add(this.panel1main);
+            this.Controls.Add(this.dgw);
+            this.Controls.Add(this.buttonexcel);
+            this.Controls.Add(this.buttoncsv);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgw)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1main;
-        private System.Windows.Forms.Button button2birds;
+        private System.Windows.Forms.Button buttoncsv;
+        private System.Windows.Forms.Button buttonexcel;
+        private System.Windows.Forms.DataGridView dgw;
     }
 }
 
